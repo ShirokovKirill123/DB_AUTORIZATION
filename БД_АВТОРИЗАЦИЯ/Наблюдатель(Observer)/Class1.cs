2e301371df09a9bd76e5789ec2e3e48ac168eb10
shirokov.ky1675 <shirokov.ky1675@M409C-1R>
2024-11-13 13:29:04 +0300

    проектирование_2

---
+++ БД_АВТОРИЗАЦИЯ/Наблюдатель(Observer)/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace БД_АВТОРИЗАЦИЯ.Наблюдатель_Observer_
+{
+    internal class Class1
+    {
+    }
+}
