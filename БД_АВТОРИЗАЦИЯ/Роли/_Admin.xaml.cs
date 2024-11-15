4bf2b5462c2fce33af0cd1e56e2a095d5d1b2608
shirokov.ky1675 <shirokov.ky1675@M409C-1R>
2024-11-15 13:32:15 +0300

    mouse

--- БД_АВТОРИЗАЦИЯ/Роли/_Admin.xaml.cs
+++ БД_АВТОРИЗАЦИЯ/Роли/_Admin.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +25,61 @@ namespace БД_АВТОРИЗАЦИЯ
         public _Admin()
         {
             InitializeComponent();
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            ___Без_имени_.Visibility = Visibility.Visible;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 1 clicked!");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 2 clicked!");
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 3 clicked!");
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 4 clicked!");
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 5 clicked!");
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 6 clicked!");
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 7 clicked!");
+        }
+
+        private void Button_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            ___Без_имени_.Visibility = Visibility.Hidden;
+        }
+
+        private void ___Без_имени__MouseEnter(object sender, MouseEventArgs e)
+        {
+            ___Без_имени_.Visibility = Visibility.Visible;
+        }
+
+        private void Button_MouseLeave_2(object sender, MouseEventArgs e)
+        {
+            ___Без_имени_.Visibility = Visibility.Hidden;
         }
     }
 }
